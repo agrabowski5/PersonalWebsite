fa3be4ad7ba7d0d30b2e2bb5b54eab4ba8d5a3e9
Andrew Grabowski <agrabowski5@gmail.com>
2025-03-23 20:05:14 -0400

    Lab_4 end

---
+++ src/lib/Project.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let data = {};
+    export let hLevel = 2;
+  </script>
+
+<article>
+    <svelte:element this={"h" + hLevel}>{data.title}</svelte:element>
+    <img src={data.image} alt="" />
+    <p>
+        {data.description}
+    </p>
+</article>
+
+<style>
+    article {
+  display: grid;
+  grid-template-rows: subgrid;
+  grid-row: span 3; /* Make each article span 3 rows */
+  padding: 1em;
+  border-radius: 0.25em;
+  box-shadow: 0 0 0.5em rgba(0, 0, 0, 0.1);
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
+<script>
+    import projects from "$lib/projects.json";
+    import Project from "$lib/Project.svelte";
+</script>
+
 <h1>Andrew Grabowski</h1>
     
 <img src="images/GrabowskiMITPhoto.jpg" alt="Andrew Grabowski" width="200" height="200">
 <p>Hi, I'm Andrew. I'm a software engineer and developing a website.</p>
+
+<h2>Latest Projects</h2>
+<div class="projects">
+    {#each projects.slice(0, 3) as p}
+    <Project data={p} />
+    {/each}
+</div>
+
