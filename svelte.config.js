524dea2fa642277dd6368f8dd9bf7efa645fe09c
Andrew Grabowski <agrabowski5@gmail.com>
2025-05-16 16:27:47 -0400

    wwent bcak to svelte original

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-const dev = process.argv.includes('dev');
-const repoName = 'Personal_Website'; // Your GitHub repository name
-const base = dev ? '' : `/${repoName}`;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-    kit: {
-        adapter: adapter({ 
-            fallback: 'index.html',
-            precompress: false
-        }),
-        paths: {
-            base: base, // Set base path explicitly
-        },
-        prerender: {
-            handleHttpError: 'warn',
-        },
-    },
+	kit: {
+		adapter: adapter({ fallback: '404.html' }),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+		},
+	},
 };
 
 export default config;
