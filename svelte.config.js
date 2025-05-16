4b003f01e98b7c5042a92a7bcc8c7b5e4ca70c4c
Andrew Grabowski <agrabowski5@gmail.com>
2025-05-16 16:07:40 -0400

    Changed it so paths are set correctly

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
+const repoName = 'Personal_Website'; // Your GitHub repository name
+const base = dev ? '' : `/${repoName}`;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({ fallback: '404.html' }),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-		},
-	},
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({ fallback: 'index.html' }), // Using index.html as fallback
+        paths: {
+            base: base, // Set base path explicitly
+        },
+        prerender: {
+            handleHttpError: 'warn',
+        },
+        trailingSlash: 'always'
+    },
 };
 
 export default config;
