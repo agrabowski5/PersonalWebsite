f38368dba349b155f4a8cddc03ecc38bf356cf4b
Andrew Grabowski <agrabowski5@gmail.com>
2025-05-16 16:33:37 -0400

    Include BasePath

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
+// Explicitly set the repository name for GitHub Pages
+const dev = process.argv.includes('dev');
+const base = dev ? '' : '/PersonalWebsite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({ fallback: '404.html' }),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-		},
-	},
+    kit: {
+        adapter: adapter({ fallback: '404.html' }),
+        paths: {
+            base: base,  // Use the explicit base path
+        },
+    },
 };
 
 export default config;
