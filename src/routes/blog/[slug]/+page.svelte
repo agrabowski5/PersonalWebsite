b452d7f38fb6847db4596bd2bb3fac384c8cacdb
Andrew Grabowski <agrabowski5@gmail.com>
2026-01-02 07:32:20 -0500

    Steel Blog Post

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -122,5 +122,71 @@ Economic simulation allows us to explore outcomes of complex systems where analy
             tags: ['economics', 'simulation', 'agent-based modeling'],
             image: '/images/blog/simulation.jpg'
         },
+        'understanding-steel-industry-green-steel': {
+            title: 'Understanding the Steel Industry and the Path to Green Steel',
+            date: '2026-01-01',
+            author: 'Andrew Grabowski',
+            content: `
+## A New Year, A New Learning Journey
+
+Today is New Year's Day 2026, and I wanted to start something new. A lot of times my brain feels broken, because although I can read something a thousand times, I don't seem to remember it unless I use it or teach it to someone else. That's why I wanted to start writing on topics that I think are interesting to me in a freeform manner without considerations for form. I just wanted to write to write, and potentially learn better along the way.
+
+My first topic of interest is going to be on the Steel industry. This industry has constantly been on the edge of my experience, always close but I've never dived in. When I was at MathWorks, I supported Boston Metal, and at World Energy we explored how to use hydrogen reserve for green steel creation, but I never sat down to learn about steel, so here we go.
+
+## The Steel Industry's Climate Impact
+
+The Steel industry is a huge emitter of CO₂ emissions with potentially 7-11% of all emissions. This shouldn't be a surprise if we think about all the value that comes from it. It got us out of the stone, and iron ages, allowing us to develop awe inspiring buildings and infrastructure (51% of all steel used), Automobiles (12%) and airplanes and trains (5%), and allowed us to make all the tools that helped us build these things. Similar to what electricity did for the digital age, steel became our mechanical chromosomal structure defining how we would be able to build and who would be able to build.
+
+### The Global Steel Landscape
+
+Today, the world's largest steel producer is China, with over 50% of steel created there. With over 1875 MMT of steel created every year, they far exceed every other nation's development. China's BaoWu group leads with 130 MMT produced, followed by Luxembourg's ArcelorMittal's 68 MMT, with again China's Ansteel Group and Japan's Nippon Steel following in fourth. The East is leading the charge, since the west has offloaded these trade heavy industry jobs to the east. Even America's Nucor is mainly a recycler of steel and US Steel is potentially getting acquired by Nippon Steels.
+
+## The Current Steel Production Process
+
+These industries are dirty, and energy intensive. Depending on the process, 1 MT of steel could require 2 tonnes of CO₂ emitted. Steel is fundamentally an alloy of iron and up to 2% carbon with other elements to obtain particular properties. For instance, higher carbon means stronger, but more brittle steel. These emissions come from both the energy intensity with the extreme heat required of 1500°C to actually melt the iron, but also the current need for carbon sources to remove the oxygen from natural iron ore.
+
+### Traditional Production Methods
+
+Before we jump into how we can clean up this industry, let's discuss how it's done today. Today it has a primary approach from virgin iron ore called **Blast Furnace plus Basic Oxygen Furnace (BF-BOF)**. We also use a secondary approach to help with scrap or recycled steel using an **Electric Arc Furnace (EAF)**.
+
+The EAF isn't a direct replacement for BF-BOF, but instead allows an accumulation of old steel and other metals to be thrown in a furnace. Once collected, a set of electrodes is inserted into the shred at the top and melted down at a lower voltage, then it moves to the deeper material and turns up the voltage to melt the entire furnace. Oxygen and coke is then pumped into the system - oxygen to remove the impurities and coke to remove the oxygen from iron oxide. This process can use electricity instead of coal or other fossil fuels to create the heat needed to get the temperature up to 1500°C which allows for forming. However, it still needs carbon to actually create the steel.
+
+## Green Steel Technologies
+
+Now, can we ask what happens if we want it to be green steel?
+
+### Molten Oxide Electrolysis (MOE)
+
+One promising approach today is explored by Boston Metal. They aim to use a Molten Oxide Electrolysis to replace the need for blast furnaces. The current setup pours iron ore into the furnace full of electrolyte, which is really just a molten mixture of metal oxides like MgO, CaO, SiO₂, and Al₂O₃ that acts as a solvent. The electrodes then pass the current through the electrolyte with the iron ore driving temperatures to 1600°C which then facilitates the separation of the iron and the oxygen gas where the metal is collected at the cathode, and the oxygen is collected at the anode. The iron is then tapped at the bottom of the furnace to get pure iron. The small percentage of carbon can then be added in later stages using things like ladle metallurgy.
+
+### Other Green Steel Approaches
+
+There are other processes as well, including using **hydrogen-based DRI**, which uses hydrogen instead of the carbon of coke to strip off oxygen creating water instead of CO₂. There are also proposed approaches of using a perovskite structure that can convert CO₂ to CO in a blast furnace, however today the MOE approach seems to be the most understood 'green steel' approach.
+
+## Market Mechanisms: Book and Claim
+
+Finally, the last thing we wanted to explore was to understand how companies like MSFT and Apple could actually use these 'green steel' options. Since supply of green steel will most likely start off in very concentrated areas such as in the Labrador Trough in Canada, a company like ArcelorMittal or Rio Tinto may have its iron ore deposits, and therefore want to have its steel refinery near the deposit level no longer needing long transportation of the iron ore. The company could use the MOE process to convert the iron ore into pure iron metal and add carbon to create middle grade steel. This steel would be termed green steel if the energy and embodied emissions of the steel is reduced compared to the baseline steel levels of around 2 MT CO₂/MT of Steel.
+
+### How Book and Claim Works
+
+If MSFT wanted to use this in Seattle, they could ship this steel out to Seattle for use, however this could be expensive and increase the CO₂ output due to the transportation required. Instead they could use **Book and Claim**. Book and Claim is a chain of custody model that lets you detach the environmental attributes of a product such as green steel from the actual physical good. This means that the steel could be sold to local steel users in Canada, but the environmental attributes of the steel could be purchased by MSFT for any uses of steel they have where they have used typical 'dirty' steel.
+
+This Book and Claim model isn't as evolved as other sustainable goods such as sustainable aviation fuel, but it is developing. This book and claims allows us to still participate in green steel markets without actually being required to ship it to us.
+
+### Challenges and Questions
+
+However, many complications arise:
+
+1. **Physical Properties**: Steel has many varieties available with high strength, compression factors, yield, and other factors. Do these physical properties have to be equivalent in order to use Book and Claim?
+
+2. **Market Development**: Will using this model hamper or help the market development?
+
+## Conclusion
+
+Through this exploration, we looked at the current state of the steel market and explored how the future may look from a technology standpoint. Overall, steel is a huge component of our economy and is critical to maintaining environmental goals. Thank you all for your time and I hope you learned something!
+            `,
+            tags: ['steel', 'green technology', 'climate', 'industrial decarbonization', 'book and claim'],
+            image: '/images/blog/green-steel.jpg'
+        },
     };
 </script>
